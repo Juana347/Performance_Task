b32f4a78e8ad2fbf41a37e5b725107faacfb7d21
Juana347 <gisellegarcia_10@icloud.com>
2026-02-11 19:11:08 +0000

    task

--- main.py
+++ main.py
@@ -2,14 +2,26 @@
 
 to_do_list = ["Homework", 'Walk the Dog', 'Eat Pizza']
 
+def intro():
+    name = input('Hello, what is your name? ')
+    print(f"Hello {name}. This is a list of things you need to finish. It currently holds three tasks but if they don't apply to you you can choose to add or remove them. ")
+    print(to_do_list)
+
+intro()
+
+def lists():
+    answer = input("Is there anything you want to add or remove? (yes/no) ")
+    if answer == "yes":
+        ans = input('Add or remove? ')
+        if ans == 'add' or 'Add':
+            input('What would you like to add? ')
+    else:
+        print("Make sure to finish your current tasks and come back when you finish one to update your list!")
 
 
-print("This is a list of things you need to finish!")
-print(to_do_list)
-to = input("Is there anything you want to add or remove?")
+lists()
 
 
-if to == 'yes':
-    input("Add or remove?")
-else: 
-    print("Make sure to finish your current tasks and come back when you finish one to update your list!")
+
+
+def add_remove(to_do_list):
